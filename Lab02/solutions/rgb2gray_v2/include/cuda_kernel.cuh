8af5022a8175d0eb24028ce5f84908b032128404
divadnauj-GB <juan.guerrero@polito.it>
2024-11-08 17:46:07 +0100

    rgb2gray sol

---
+++ Lab02/solutions/rgb2gray_v2/include/cuda_kernel.cuh
@@ -0,0 +1,12 @@
+
+#include "img.h"
+// List wrapper function callable by .cpp file.
+void kernel_rgb2gray(RGBImage *Input, GrayImage *Output);
+
+
+
+struct pixel{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    };
